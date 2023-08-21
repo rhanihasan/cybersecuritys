ddf4ac8e48476d5cacbf9682872881d50e17679c
rhanihasan <rhanihasan@gmail.com>
2023-08-21 22:57:26 +0530

    namechanged

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/myjourney/docs/category/cybersecurity">
+            to="/journey/docs/category/cybersecurity">
             Let Start With CyberSecurity ...
           </Link>
         </div>
